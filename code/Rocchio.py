94ddadefe9055f37f9c12d07211783263672a144
Roci <56322127+rocioog00@users.noreply.github.com>
2022-01-13 23:35:42 -0500

    Update Rocchio.py

--- code/Rocchio.py
+++ code/Rocchio.py
@@ -1,15 +1,10 @@
 def sum(vec1,vec2):
-    sum={}
-    for i in vec1.keys():
-        if i in vec2:
-            sum[i]=vec1[i]+vec2[i]
+    for i in vec2:
+        if i in vec1:
+            vec1[i]+=vec2[i]
 
         else:
-            sum[i]=vec1[i]
-
-    for i in vec2.keys():
-        if not (i in sum):
-            sum[i]=vec2[i]
+            vec1[i]=vec2[i]
 
 def mult(vec,k):
     mult={}
@@ -17,15 +12,15 @@ def mult(vec,k):
         mult[i]=k*vec[i]
 
 def Rocchio(query,docs_rel,docs_no_rel,alpha,beta,ganma):
-    Doc_rel=docs_rel[0]
-    for i in range(1,len(docs_rel)):
-        sum(Doc_rel,i)
+    Doc_rel={}
+    for i in docs_rel:
+        Doc_rel=sum(Doc_rel,i)
 
     Doc_rel=mult(Doc_rel,(beta*(1/len(docs_rel))))
 
-    Doc_no_rel=docs_no_rel[0]
-    for i in range(1,len(docs_no_rel)):
-        sum(Doc_no_rel,i)
+    Doc_no_rel={}
+    for i in docs_no_rel:
+        Doc_no_rel=sum(Doc_no_rel,i)
 
     Doc_no_rel=mult(Doc_no_rel,(ganma*(1/len(docs_no_rel))))
 
