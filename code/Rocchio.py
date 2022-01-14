c9a8e88709aad028d57c0defb5e1f0574054de69
CTS-crypto <72420685+CTS-crypto@users.noreply.github.com>
2022-01-14 00:25:21 -0500

    ultimas cosas

--- code/Rocchio.py
+++ code/Rocchio.py
@@ -18,13 +18,15 @@ def Rocchio(query,docs_rel,docs_no_rel,alpha,beta,ganma):
     for i in docs_rel:
         sum(Doc_rel,i)
 
-    Doc_rel=mult(Doc_rel,(beta*(1/len(docs_rel))))
+    if len(docs_rel)!=0:
+        Doc_rel=mult(Doc_rel,(beta*(1/len(docs_rel))))
 
     Doc_no_rel={}
     for i in docs_no_rel:
         sum(Doc_no_rel,i)
 
-    Doc_no_rel=mult(Doc_no_rel,(ganma*(-1/len(docs_no_rel))))
+    if len(docs_no_rel)!=0:
+        Doc_no_rel=mult(Doc_no_rel,(ganma*(-1/len(docs_no_rel))))
 
     doc=sum(Doc_rel,Doc_no_rel)
     qm=sum(doc,mult(query,alpha))
